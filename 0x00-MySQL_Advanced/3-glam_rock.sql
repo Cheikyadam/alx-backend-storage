cedc5442363dd958a15771b9a96955e0643aac70
Cheikyadam <urbaingande@gmail.com>
2024-07-11 21:46:54 +0000

    2

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,11 @@
+-- lifespan
+
+SELECT
+    band_name,
+    IF(split IS NULL, 2022 - formed, split - formed) AS lifespan
+FROM
+    metal_bands
+WHERE
+     style LIKE '%Glam rock%'
+ORDER BY
+    lifespan DESC;
