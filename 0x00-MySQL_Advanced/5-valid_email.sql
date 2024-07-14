5adfd7f633f5d6100a200506d3da28c191ee8bf5
Cheikyadam <urbaingande@gmail.com>
2024-07-14 09:00:48 +0000

    5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,16 @@
+-- trigger validate email
+
+DELIMITER //
+
+CREATE TRIGGER valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email != OLD.email THEN
+           SET NEW.valid_email = 0;
+    	END IF;
+END;
+
+//
+
+DELIMITER ;
