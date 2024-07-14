66a21392e1d8b01771c1b7ae3d8f9018193203a7
Cheikyadam <urbaingande@gmail.com>
2024-07-14 18:55:04 +0000

    101

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,30 @@
+-- for all users
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE user_id INT;
+
+    DECLARE cur CURSOR FOR SELECT id FROM users;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    OPEN cur;
+
+    read_loop: LOOP
+        FETCH cur INTO user_id;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        CALL ComputeAverageWeightedScoreForUser(user_id);
+    END LOOP;
+
+    CLOSE cur;
+END;
+
+//
+
+DELIMITER ;
+
